9dea676414da3377c6b02b44ab95bad574364441
Jatin Mishra <104919571+Jatinmishra02@users.noreply.github.com>
2024-02-20 21:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function stringChop(str, size) {
-  // your code here
+  if (!str) return []; // If the input string is null, return an empty array
+  const chunks = [];
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.slice(i, i + size));
+  }
+  return chunks;
 }
 
-// Do not change the code below
+// Example
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(JSON.stringify(stringChop(str, parseInt(size))));
